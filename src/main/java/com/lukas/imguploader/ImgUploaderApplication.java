f1d5f3b61a9c7d02bbd8f453c022aac98aad021c
Evgeniy <46program@gmail.com>
2022-06-21 09:03:24 +0300

    Initial commit

---
+++ src/main/java/com/lukas/imguploader/ImgUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.lukas.imguploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukas/imguploader/ImgUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukas.imguploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
